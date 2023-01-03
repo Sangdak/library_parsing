fbc203f69f84a5c6d86611467cd57d86c8b166a6
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-01-04 02:36:57 +1000

    lsn_3__stp_3/16 - download books from tululu.org.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
+from pathlib import Path
 
 
-url = 'https://tululu.org/txt.php?id=32168'
+def download_books(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
 
-response = requests.get(url)
-response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
-filename = 'sand.txt'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+dir_name = 'books'
+for book_id in range(1, 11):
+    Path(f'./{dir_name}').mkdir(parents=True, exist_ok=True)
+    link_pattern = f'https://tululu.org/txt.php?id={book_id}'
+    output = Path(f'./{dir_name}/id{book_id}.txt')
+    download_books(link_pattern, output)
